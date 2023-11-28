5e1c06c66501c9b9c33fd2cb0f628dafdc2a0a20
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-11-28 20:30:51 +0300

    fix seller active status

--- packages/Organon/Marketplace/src/Enums/SellerStatusEnum.php
+++ packages/Organon/Marketplace/src/Enums/SellerStatusEnum.php
@@ -5,7 +5,7 @@ namespace Organon\Marketplace\Enums;
 enum SellerStatusEnum : string
 {
     case PENDING = 'pending-approval';
-    case ACTIVE = 'approved';
+    case ACTIVE = 'active';
     case DEACTIVATED = 'deactivated';
 
 }
