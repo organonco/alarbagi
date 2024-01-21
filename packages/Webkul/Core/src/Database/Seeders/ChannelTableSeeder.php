f680bef134bb1c53c47b92359d7b26e1a7d8f243
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-01-22 02:45:42 +0300

    fix demo store title

--- packages/Webkul/Core/src/Database/Seeders/ChannelTableSeeder.php
+++ packages/Webkul/Core/src/Database/Seeders/ChannelTableSeeder.php
@@ -27,7 +27,7 @@ class ChannelTableSeeder extends Seeder
                 'channel_id'        => 1,
                 'locale'            => 'en',
                 'name'              => 'Default',
-                'home_seo'          => '{"meta_title": "Demo store", "meta_keywords": "Demo store meta keyword", "meta_description": "Demo store meta description"}',
+                'home_seo'          => '{"meta_title": "Souq Naif", "meta_keywords": "Souq Naif", "meta_description": "Souq Naif"}',
             ],
         ]);
 
