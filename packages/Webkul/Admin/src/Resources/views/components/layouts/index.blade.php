d7fa2d1afe3aa8882ec754caa1cd4e627311c877
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-01-31 01:21:17 +0300

    fix icons

--- packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
@@ -25,8 +25,10 @@
             rel="stylesheet"
         />
 
-        <link rel="icon" href="{{asset('assets/images/favicon.ico')}}" sizes="any">
-        <link rel="apple-touch-icon" href="{{asset('assets/images/apple-touch-icon.png')}}">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
 
         @stack('styles')
 
