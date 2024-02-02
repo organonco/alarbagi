4159228dda9aa7b50726ca9263b2982bdf5706ec
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-02-02 22:56:50 +0300

    remove image icon from search (mobile)

--- packages/Webkul/Shop/src/Resources/views/components/layouts/header/mobile/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/header/mobile/index.blade.php
@@ -269,13 +269,6 @@
                     placeholder="@lang('shop::app.components.layouts.header.search-text')"
                     required
             >
-
-            <button
-                    type="button"
-                    class="icon-camera flex items-center absolute top-[12px] right-[12px] pr-3 text-[22px]"
-                    aria-label="Search"
-            >
-            </button>
         </div>
     </form>
 </div>
