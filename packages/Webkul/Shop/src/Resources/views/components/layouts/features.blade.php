14e1bfc120ae5b8efc75bab22a6efaaca868a972
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-02-25 01:09:53 +0300

    fix features colors

--- packages/Webkul/Shop/src/Resources/views/components/layouts/features.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/features.blade.php
@@ -4,10 +4,10 @@
 <div class="container mt-20 max-lg:px-[30px] max-sm:mt-[30px]">
     <div class="flex gap-[25px] justify-center max-lg:flex-wrap">
         <div class="flex items-center gap-[20px]">
-            <span class="flex items-center justify-center w-[60px] h-[60px] bg-white border border-black rounded-full text-[42px] text-navyBlue p-[10px] {{trans('shop::app.footer.0.icon')}}"></span>
+            <span class="flex items-center justify-center w-[60px] h-[60px] bg-white border sn-border-main rounded-full text-[42px] text-navyBlue p-[10px] {{trans('shop::app.footer.0.icon')}} sn-color-light-main"></span>
 
             <div class="">
-                <p class="text-[16px] font-medium font-dmserif">{{trans('shop::app.footer.0.title')}}</p>
+                <p class="text-[16px] font-medium font-dmserif sn-color-light-main">{{trans('shop::app.footer.0.title')}}</p>
 
                 <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">
                     {{trans('shop::app.footer.0.text')}}
@@ -16,11 +16,9 @@
         </div>
 
         <div class="flex items-center gap-[20px]">
-            <span class="flex items-center justify-center w-[60px] h-[60px] bg-white border border-black rounded-full text-[42px] text-navyBlue p-[10px] {{trans('shop::app.footer.1.icon')}}"></span>
-
+            <span class="flex items-center justify-center w-[60px] h-[60px] bg-white border sn-border-main rounded-full text-[42px] text-navyBlue p-[10px] {{trans('shop::app.footer.1.icon')}} sn-color-light-main"></span>
             <div class="">
-                <p class="text-[16px] font-medium font-dmserif">{{trans('shop::app.footer.1.title')}}</p>
-
+                <p class="text-[16px] font-medium font-dmserif sn-color-light-main">{{trans('shop::app.footer.1.title')}}</p>
                 <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">
                     {{trans('shop::app.footer.1.text')}}
                 </p>
@@ -28,10 +26,10 @@
         </div>
 
         <div class="flex items-center gap-[20px]">
-            <span class="flex items-center justify-center w-[60px] h-[60px] bg-white border border-black rounded-full text-[42px] text-navyBlue p-[10px] {{trans('shop::app.footer.2.icon')}}"></span>
+            <span class="flex items-center justify-center w-[60px] h-[60px] bg-white border sn-border-main rounded-full text-[42px] text-navyBlue p-[10px] {{trans('shop::app.footer.2.icon')}} sn-color-light-main"></span>
 
             <div class="">
-                <p class="text-[16px] font-medium font-dmserif">{{trans('shop::app.footer.2.title')}}</p>
+                <p class="text-[16px] font-medium font-dmserif sn-color-light-main">{{trans('shop::app.footer.2.title')}}</p>
 
                 <p class="text-[14px] font-medium mt-[10px] text-[#6E6E6E] max-w-[217px]">
                     {{trans('shop::app.footer.2.text')}}
