969959c694d6ab7d4fd940fb3b58077cbe193536
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-06-01 03:40:51 +0300

    fix order status

--- packages/Organon/Marketplace/src/Notifications/Repositories/SellerOrderRepository.php
+++ packages/Organon/Marketplace/src/Notifications/Repositories/SellerOrderRepository.php
@@ -4,6 +4,7 @@ namespace Organon\Marketplace\Notifications\Repositories;
 
 use Organon\Marketplace\Contracts\SellerOrder;
 use Organon\Marketplace\Enums\SellerOrderStatusEnum;
+use Organon\Marketplace\Models\Order;
 use Webkul\Core\Eloquent\Repository;
 
 class SellerOrderRepository extends Repository
@@ -37,6 +38,7 @@ class SellerOrderRepository extends Repository
     public function approve(SellerOrder $sellerOrder)
     {
         $sellerOrder->setStatus(SellerOrderStatusEnum::APPROVED);
+        $sellerOrder->order->update(['status' => Order::STATUS_PROCESSING]);
     }
 
     /**
@@ -46,6 +48,13 @@ class SellerOrderRepository extends Repository
     public function cancel(SellerOrder $sellerOrder)
     {
         $sellerOrder->setStatus(SellerOrderStatusEnum::CANCELLED_BY_SELLER);
+        $cancelledCount = 0;
+        $allCount = $sellerOrder->order->sellerOrders()->count();
+        foreach ($sellerOrder->order->sellerOrders as $sellerOrder)
+            if ($sellerOrder->status == SellerOrderStatusEnum::CANCELLED_BY_SELLER)
+                $cancelledCount++;
+        if ($cancelledCount == $allCount)
+            $sellerOrder->order->update(['status' => Order::STATUS_CANCELED]);
     }
 
 
