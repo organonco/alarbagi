b0ef20acf46a78b7b1e722ed2045ba9ce43f6c5d
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-07-03 23:08:41 +0300

    add offer component

--- packages/Organon/Marketplace/src/Resources/views/admin/offers/preview.blade.php
+++ packages/Organon/Marketplace/src/Resources/views/admin/offers/preview.blade.php
@@ -14,24 +14,7 @@
 
 
         <div class="flex gap-[10px] mt-[14px] max-xl:flex-wrap">
-
-            <div class=" flex flex-col gap-[8px] flex-1 max-xl:flex-auto">
-                <div class="p-[16px] bg-white dark:bg-gray-900 rounded-[4px] box-shadow">
-                    <div class="w-full flex justify-center" >
-                        <div class="bg-white p-[16px] flex flex-col gap-[8px]" style="border: 1px solid black;">
-                            @if($model->title)
-                                <div class="text-center" style="font-size: 40px">{{$model->title}}</div>
-                            @endif
-                            @if($model->post)
-                                <div class="text-center">{{$model->post}}</div>
-                            @endif
-                            @if($model->image_url)
-                                <img src="{{$model->image_url}}" style="height: 200px; width: auto; padding-top: 20px"/>
-                            @endif
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-offer :title="$model->title" :post="$model->post" :image="$model->image_url"></x-offer>
         </div>
 
 
