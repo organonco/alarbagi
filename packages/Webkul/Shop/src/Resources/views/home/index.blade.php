12c7744b72d07968d4d5659ba3d4fdfcdb077940
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-07-17 23:51:00 +0300

    make home page responsive

--- packages/Webkul/Shop/src/Resources/views/home/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/home/index.blade.php
@@ -20,18 +20,19 @@
     </x-slot>
     
     {{-- TOP BANNER --}}
-    <div class="bg-[#FF6B30] h-[30rem] flex justify-between">
-        <div class="mr-60 h-full flex justify-center flex-col gap-12">
-            <div class="sn-color-primary text-6xl font-extrabold">
+    <div class="bg-[#FF6B30] h-[30rem] flex justify-between max-lg:flex-col-reverse max-lg:pb-8 max-lg:items-center max-lg:h-[55rem] max-lg:justify-center">
+        {{--  --}}
+        <div class="max-lg:mr-0 mr-60 h-full flex justify-center flex-col gap-12 max-lg:h-[20rem]">
+            <div class="sn-color-primary text-6xl font-extrabold max-lg: text-center max-lg:text-5xl max-lg:px-20 !leading-[1.3]">
                 المحل محلك وانت بمحلك
             </div>
-            <div class="sn-heading-1 sn-color-primary flex justify-center gap-3">
-                <div class="border-white rounded-md border-2 p-2 sn-heading-3 px-8 flex justify-around gap-2 items-center">
+            <div class="sn-heading-1 sn-color-primary flex justify-center gap-3 max-lg:flex-col items-center">
+                <div class="border-white rounded-md border-2 py-2 sn-heading-3 px-8 flex justify-around gap-2 items-center">
                     <div class="flex flex-col gap-1 justify-center px-4">
                         <div class="text-white text-center">
                             قم بتنزيل التطبيق
                         </div>
-                        <div class="text-white  text-center sn-heading-3 px-4">
+                        <div class="text-white  text-center sn-heading-3 px-4 max-lg:px-1">
                             Google Play
                         </div>
                     </div>
@@ -50,8 +51,8 @@
                 </div>
             </div>
         </div>
-        <div class="h-full ml-44 p-3">
-            <img class="h-full" src="{{asset('assets/images/header.png')}}"> 
+        <div class="h-full ml-44 p-3 max-lg:ml-0 max-lg:h-[30rem]">
+            <img class="h-full max-lg:h-[30rem]" src="{{asset('assets/images/header.png')}}"> 
         </div>
     </div>
     {{-- END TOP BANNER --}}
