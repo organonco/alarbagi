b387307effc51983c698f5b63a945fa5ed47fdaf
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-07-17 22:42:57 +0300

    add area seeder

---
+++ database/seeders/AreaSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Organon\Delivery\Models\Area;
+
+class AreaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $areas = [
+            'أشرفية صحنايا', 
+            'التل',
+            'المعضمية',
+            'النبك',
+            'جديدة عرطوز البلد',
+            'دمر',
+            'دير عطية',
+            'صحنايا',
+            'ضاحية الأسد',
+            'ضاحية قدسيا',
+            'قدسيا',
+            'مشروع دمر',
+            'معربا',
+            'يبرود'
+        ];
+
+        foreach($areas as $name){
+            $area = Area::create([
+                'name' => $name,
+                'info' => '',
+                'is_active' => true
+            ]);
+            $area->addMediaFromDisk('images/areas/' . $name . '.png')->preservingOriginal()->toMediaCollection('image', 'public');
+        }
+    }
+}
