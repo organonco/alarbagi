0dd83c64c47f93cc7c0ec6417c1d5e9a610e507e
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-07-21 23:28:16 +0300

    fix backgrounds

--- packages/Webkul/Shop/src/Resources/views/areas/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/areas/view.blade.php
@@ -8,29 +8,30 @@
     <x-slot:title>
         {{ $area->name }}
     </x-slot>
+    <div class="sn-background-light-grey">
+        <div class="w-full">
+            <img class="w-full" src="{{ $area->banner_url }}" />
+        </div>
 
-    <div class="w-full">
-        <img class="w-full" src="{{ $area->banner_url }}" />
+        <div class="w-full flex justify-center pt-10">
+            <img src="{{ $area->image_url }}" class="w-72 h-72 rounded-full">
+        </div>
+
+        <div class="sn-heading-1 text-center sn-color-primary pt-4">
+            {{ $area->name }}
+        </div>
+
+        <div class="flex gap-6 px-24 py-36 flex-wrap justify-center max-lg:px-6">
+            @foreach ($categories as $category)
+                <a href="{{ route('seller-category.view', ['areaId' => $area->id, 'sellerCategoryId' => $category->id]) }}"
+                    class="items-center flex gap-8 sn-background-light-green px-4 py-4 rounded-lg min-w-[450px] max-lg:min-w-full">
+                    <img src="{{ $category->image_url }}" class="w-20 h-20 rounded-full">
+                    <div
+                        class="sn-color-primary text-center font-black text-2xl w-full max-lg:text-right max-lg:text-xl">
+                        {{ $category->name }}
+                    </div>
+                </a>
+            @endforeach
+        </div>
     </div>
-
-    <div class="w-full flex justify-center pt-10">
-        <img src="{{ $area->image_url }}" class="w-72 h-72 rounded-full">
-    </div>
-
-    <div class="sn-heading-1 text-center sn-color-primary pt-4">
-        {{ $area->name }}
-    </div>
-
-    <div class="flex gap-6 px-24 py-36 flex-wrap justify-center max-lg:px-6">
-        @foreach ($categories as $category)
-            <a href="{{ route('seller-category.view', ['areaId' => $area->id, 'sellerCategoryId' => $category->id]) }}"
-                class="items-center flex gap-8 sn-background-light-green px-4 py-4 rounded-lg min-w-[450px] max-lg:min-w-full">
-                <img src="{{ $category->image_url }}" class="w-20 h-20 rounded-full">
-                <div class="sn-color-primary text-center font-black text-2xl w-full max-lg:text-right max-lg:text-xl">
-                    {{ $category->name }}
-                </div>
-            </a>
-        @endforeach
-    </div>
-
 </x-shop::layouts>
--- packages/Webkul/Shop/src/Resources/views/sellerCategories/viewChild.blade.php
+++ packages/Webkul/Shop/src/Resources/views/sellerCategories/viewChild.blade.php
@@ -8,29 +8,31 @@
     <x-slot:title>
         {{ $area->name . ' - ' . $sellerCategory->name }}
     </x-slot>
+    <div class="sn-background-light-grey">
+        <div class="w-full">
+            <img class="w-full" src="{{ $sellerCategory->banner_url }}" />
+        </div>
 
-    <div class="w-full">
-        <img class="w-full" src="{{ $sellerCategory->banner_url }}" />
-    </div>
+        <div class="w-full flex justify-center pt-10">
+            <img src="{{ $sellerCategory->image_url }}" class="w-72 h-72 rounded-full">
+        </div>
 
-    <div class="w-full flex justify-center pt-10">
-        <img src="{{ $sellerCategory->image_url }}" class="w-72 h-72 rounded-full">
-    </div>
+        <div class="sn-heading-1 text-center sn-color-primary pt-4">
+            {{ $area->name . ' - ' . $sellerCategory->name }}
+        </div>
 
-    <div class="sn-heading-1 text-center sn-color-primary pt-4">
-        {{ $area->name . ' - ' . $sellerCategory->name }}
-    </div>
-
-    <div class="flex gap-6 px-24 py-36 flex-wrap justify-center max-lg:px-6">
-        @foreach($sellers as $seller)
-            <a href="{{ route('shop.marketplace.show', ['slug' => $seller->slug]) }}"
-                class="items-center flex gap-8 sn-background-light-green px-4 py-4 rounded-lg min-w-[450px] max-lg:min-w-full">
-                <img src="{{ $seller->logo_url }}" class="w-20 h-20 rounded-full">
-                <div class="sn-color-primary text-center font-black text-2xl w-full max-lg:text-right max-lg:text-xl">
-                    {{ $seller->name }}
-                </div>
-            </a>
-        @endforeach
+        <div class="flex gap-6 px-24 py-36 flex-wrap justify-center max-lg:px-6">
+            @foreach ($sellers as $seller)
+                <a href="{{ route('shop.marketplace.show', ['slug' => $seller->slug]) }}"
+                    class="items-center flex gap-8 sn-background-light-green px-4 py-4 rounded-lg min-w-[450px] max-lg:min-w-full">
+                    <img src="{{ $seller->logo_url }}" class="w-20 h-20 rounded-full">
+                    <div
+                        class="sn-color-primary text-center font-black text-2xl w-full max-lg:text-right max-lg:text-xl">
+                        {{ $seller->name }}
+                    </div>
+                </a>
+            @endforeach
+        </div>
     </div>
 
 </x-shop::layouts>
