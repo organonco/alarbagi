93e2d3b72e5ec45c6a97903b56eb2115f91edb0c
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-08-09 05:28:29 +0300

    add secondary pages to account

--- packages/Webkul/Shop/src/Resources/views/components/layouts/account/navigation.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/account/navigation.blade.php
@@ -16,15 +16,14 @@
     $customer = auth()->guard('customer')->user();
 @endphp
 
-<div class="panel-side grid grid-cols-[1fr] gap-[30px] max-w-[380px] max-h-[1320px] overflow-y-auto overflow-x-hidden journal-scroll min-w-[342px] max-xl:min-w-[270px] max-md:max-w-full">
+<div
+    class="panel-side grid grid-cols-[1fr] gap-[30px] max-w-[380px] max-h-[1320px] overflow-y-auto overflow-x-hidden journal-scroll min-w-[342px] max-xl:min-w-[270px] max-md:max-w-full">
     {{-- Account Profile Hero Section --}}
-    <div class="grid grid-cols-[auto_1fr] gap-[15px] items-center px-[20px] py-[25px] border border-[#E9E9E9] rounded-[12px]">
+    <div
+        class="grid grid-cols-[auto_1fr] gap-[15px] items-center px-[20px] py-[25px] border border-[#E9E9E9] rounded-[12px]">
         <div class="">
-            <img
-                src="{{ $customer->image_url ??  bagisto_asset('images/user-placeholder.png') }}"
-                class="w-[60px] h-[60px] rounded-full"
-                alt="Profile Image"
-            >
+            <img src="{{ $customer->image_url ?? bagisto_asset('images/user-placeholder.png') }}"
+                class="w-[60px] h-[60px] rounded-full" alt="Profile Image">
         </div>
 
         <div class="flex flex-col justify-between">
@@ -36,26 +35,34 @@
 
     {{-- Account Navigation Menus --}}
     @foreach ($menu->items as $menuItem)
-        <div class="max-md:border max-md:border-t-0 max-md:border-r-[1px] max-md:border-l-[1px] max-md:border-b-[1px] max-md:border-[#E9E9E9]   max-md:rounded-[6px]">
+        <div
+            class="max-md:border max-md:border-t-0 max-md:border-r-[1px] max-md:border-l-[1px] max-md:border-b-[1px] max-md:border-[#E9E9E9]   max-md:rounded-[6px]">
             <v-account-navigation>
                 {{-- Account Navigation Toggler --}}
-                <div class="max-md:flex max-md:gap-x-[15px] max-md:justify-between max-md:items-center pb-[20px] max-md:bg-gray-200 max-md:px-[25px] max-md:py-[20px] max-md:rounded-tl-[6px] max-md:rounded-tr-[6px] accordian-toggle md:pointer-events-none select-none">
+                <div
+                    class="max-md:flex max-md:gap-x-[15px] max-md:justify-between max-md:items-center pb-[20px] max-md:bg-gray-200 max-md:px-[25px] max-md:py-[20px] max-md:rounded-tl-[6px] max-md:rounded-tr-[6px] accordian-toggle md:pointer-events-none select-none">
                     <p class="text-[20px] md:font-medium">@lang($menuItem['name'])</p>
 
                     <span class="icon-arrow-left text-[24px] md:hidden"></span>
                 </div>
 
                 {{-- Account Navigation Content --}}
-                <div class="grid border border-t-0 border-r-[1px] border-l-[1px] border-b-[1px] border-[#E9E9E9] rounded-[6px] max-md:hidden max-md:border-none accordian-content">
-                    @if (! (bool) core()->getConfigData('general.content.shop.wishlist_option'))
+                <div
+                    class="grid border border-t-0 border-r-[1px] border-l-[1px] border-b-[1px] border-[#E9E9E9] rounded-[6px] max-md:hidden max-md:border-none accordian-content">
+                    @if (!(bool) core()->getConfigData('general.content.shop.wishlist_option'))
                         @php
                             unset($menuItem['children']['wishlist']);
                         @endphp
                     @endif
 
+                    @php
+                        $pages = Webkul\CMS\Models\CmsPage::whereHas('translations')->get();
+                    @endphp
+
                     @foreach ($menuItem['children'] as $subMenuItem)
                         <a href="{{ $subMenuItem['url'] }}">
-                            <div class="flex justify-between px-[25px] py-[20px] border-t-[1px] border-[#E9E9E9] hover:bg-[#f3f4f682] cursor-pointer {{ request()->routeIs($subMenuItem['route']) ? 'bg-gray-100' : '' }}">
+                            <div
+                                class="flex justify-between px-[25px] py-[20px] border-t-[1px] border-[#E9E9E9] hover:bg-[#f3f4f682] cursor-pointer {{ request()->routeIs($subMenuItem['route']) ? 'bg-gray-100' : '' }}">
                                 <p class="flex gap-x-[15px] items-center text-[18px] font-medium">
                                     <span class="{{ $subMenuItem['icon'] }}  text-[24px]"></span>
 
@@ -63,6 +70,14 @@
                                 </p>
 
                                 <span class="icon-arrow-left text-[24px] max-md:hidden"></span>
+                            </div>
+                        </a>
+                    @endforeach
+					@foreach ($pages as $page)
+                        <a href="{{ route('shop.cms.page', $page->translations[0]->url_key) }}">
+                            <div
+                                class="flex justify-between px-[25px] py-[20px] border-t-[1px] border-[#E9E9E9] hover:bg-[#f3f4f682] cursor-pointer {{ request()->routeIs(route('shop.cms.page', $page->translations[0]->url_key)) ? 'bg-gray-100' : '' }}">
+                                {{ $page->translations[0]->page_title }}
                             </div>
                         </a>
                     @endforeach
@@ -97,7 +112,7 @@
 
             methods: {
                 toggleAccordion() {
-                    this.isOpen = ! this.isOpen;
+                    this.isOpen = !this.isOpen;
 
                     if (this.isOpen) {
                         this.$el.querySelector('.icon-arrow-left').classList.add('icon-arrow-down');
@@ -113,5 +128,5 @@
                 },
             },
         });
-      </script>
+    </script>
 @endpushOnce
