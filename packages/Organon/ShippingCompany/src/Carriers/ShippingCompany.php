cdd40af47f104ac1f90998439681dcd61e0ed14b
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-08-21 02:23:25 +0300

    fix shipping cost

--- packages/Organon/ShippingCompany/src/Carriers/ShippingCompany.php
+++ packages/Organon/ShippingCompany/src/Carriers/ShippingCompany.php
@@ -10,101 +10,101 @@ use Webkul\Shipping\Facades\Shipping;
 
 class ShippingCompany extends AbstractShipping
 {
-    /**
-     * Shipping method code
-     *
-     * @var string
-     */
-    protected $code  = 'shippingcompany';
+	/**
+	 * Shipping method code
+	 *
+	 * @var string
+	 */
+	protected $code  = 'shippingcompany';
 
-    /**
-     * @param int $price
-     * @return CartShippingRate
-     */
-    private function getCartShippingRateObject(): CartShippingRate
-    {
-        $object = new CartShippingRate;
+	/**
+	 * @param int $price
+	 * @return CartShippingRate
+	 */
+	private function getCartShippingRateObject(): CartShippingRate
+	{
+		$object = new CartShippingRate;
 
-        $object->carrier = 'shippingcompany';
-        $object->carrier_title = '';
-        $object->method = 'shippingcompany_shippingcompany';
-        $object->method_title = $this->getConfigData('title');
-        $object->method_icon = $this->getIcon();
+		$object->carrier = 'shippingcompany';
+		$object->carrier_title = '';
+		$object->method = 'shippingcompany_shippingcompany';
+		$object->method_title = $this->getConfigData('title');
+		$object->method_icon = $this->getIcon();
 
-        return $object;
-    }
+		return $object;
+	}
 
-    private function generateUnavailableObject(string $key)
-    {
-        return [
-            'isAvailable' => false,
-            'reason' => trans('shipping-company::app.messages.' . $key)
-        ];
-    }
+	private function generateUnavailableObject(string $key)
+	{
+		return [
+			'isAvailable' => false,
+			'reason' => trans('shipping-company::app.messages.' . $key)
+		];
+	}
 
-    private function checkAvailability()
-    {
-        $cart = Cart::getCart();
-        $shippingAddress = $cart->shipping_address;
+	private function checkAvailability()
+	{
+		$cart = Cart::getCart();
+		$shippingAddress = $cart->shipping_address;
 
-        if (!$cart->hasDeliverableItems())
-            return $this->generateUnavailableObject('no-deliverable-items');
-        if (is_null($shippingAddress->area_id) || is_null($shippingAddress->address_details))
-            return $this->generateUnavailableObject('address-or-area-not-found');
-        if (!$shippingAddress->area->is_shippable)
-            return $this->generateUnavailableObject('delivery-not-available-to-this-area');;
+		if (!$cart->hasDeliverableItems())
+			return $this->generateUnavailableObject('no-deliverable-items');
+		if (is_null($shippingAddress->area_id) || is_null($shippingAddress->address_details))
+			return $this->generateUnavailableObject('address-or-area-not-found');
+		if (!$shippingAddress->area->is_shippable)
+			return $this->generateUnavailableObject('delivery-not-available-to-this-area');;
 
-        return [
-            'isAvailable' => true,
-            'sameArea' => $this->sameArea($cart->shipping_address->area_id, $cart)
-        ];
-    }
-
-
-    private function sameArea(int $areaId, $cart)
-    {
-        foreach ($cart->items as $item)
-            if ($item->product->getSellerAreaId() != $areaId)
-                return false;
-        return true;
-    }
+		return [
+			'isAvailable' => true,
+			'sameArea' => $this->sameArea($cart->shipping_address->area_id, $cart)
+		];
+	}
 
 
-    private function getShippingPrice(): int
-    {
-        $cart = Cart::getCart();
-        $company = $cart->shipping_address->area->shippingCompany;
-        $price = $company['per_order_price'];
-        foreach ($cart->items as $item) {
-            $price += $item->quantity * $company['per_product_price'];
-        }
-        return $price;
-    }
+	private function sameArea(int $areaId, $cart)
+	{
+		foreach ($cart->items as $item)
+			if ($item->product->getSellerAreaId() != $areaId)
+				return false;
+		return true;
+	}
 
-    /**
-     * @return CartShippingRate|false
-     */
-    public function calculate()
-    {
-        $object = $this->getCartShippingRateObject();
-        $availability = $this->checkAvailability();
 
-        $object->is_available = $availability['isAvailable'];
+	private function getShippingPrice(): int
+	{
+		$cart = Cart::getCart();
+		$companies = collect([]);
+		foreach ($cart->items as $item)
+			$companies->push($item->product->seller_id);
 
-        if ($object->is_available && $availability['sameArea']) {
-                $price = $this->getShippingPrice();
-                $object->price = $price;
-                $object->base_price = $price;
-                $object->method_description = $this->getConfigData('description');
-        } elseif($object->is_available) {
-            $price = 0;
-            $object->price = $price;
-            $object->base_price = $price;
-            $object->method_description = trans('shipping-company::app.messages.price-defined-later');
-        } else {
-            $object->method_description = $availability['reason'];
-        }
+		$company = $cart->shipping_address->area->shippingCompany;
+		return $company['per_order_price'] + ($company['per_product_price'] * $companies->unique()->count());
+	}
 
-        return $object;
-    }
+	/**
+	 * @return CartShippingRate|false
+	 */
+	public function calculate()
+	{
+		$object = $this->getCartShippingRateObject();
+		$availability = $this->checkAvailability();
+
+		$object->is_available = $availability['isAvailable'];
+
+		if ($object->is_available && $availability['sameArea']) {
+			$price = $this->getShippingPrice();
+			$object->price = $price;
+			$object->base_price = $price;
+			$object->method_description = $this->getConfigData('description');
+		} elseif ($object->is_available) {
+			$price = 0;
+			$object->price = $price;
+			$object->base_price = $price;
+			$object->method_description = trans('shipping-company::app.messages.price-defined-later');
+		} else {
+			$object->method_description = $availability['reason'];
+		}
+
+		return $object;
+	}
 }
