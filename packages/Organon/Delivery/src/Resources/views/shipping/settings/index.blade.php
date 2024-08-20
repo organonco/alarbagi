301f412936659b6322f1c25e0f08771abc624d3e
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2024-08-21 02:30:05 +0300

    fix translation

--- packages/Organon/Delivery/src/Resources/views/shipping/settings/index.blade.php
+++ packages/Organon/Delivery/src/Resources/views/shipping/settings/index.blade.php
@@ -1,21 +1,21 @@
 <x-delivery::layouts.shipping>
     <div class="card">
         <div class="header">
-            رسوم التوصيل الحالية
+            أمثلة عن رسوم التوصيل الحالية
         </div>
         <hr/>
         <div class="content">
             <div>
-                توصيل منتج واحد: {{$per_order_price + ($per_product_price)}} ل.س
+                توصيل من تاجر واحد: {{$per_order_price + ($per_product_price)}} ل.س 
             </div>
             <div>
-                توصيل منتجين : {{$per_order_price + ($per_product_price * 2)}} ل.س
+                توصيل من تاجرين : {{$per_order_price + ($per_product_price * 2)}} ل.س
             </div>
             <div>
-                توصيل  3 منتجات: {{$per_order_price + ($per_product_price * 3)}} ل.س
+                توصيل  من 3 تجار: {{$per_order_price + ($per_product_price * 3)}} ل.س
             </div>
             <div>
-                توصيل  4منتجات: {{$per_order_price + ($per_product_price * 4)}} ل.س
+                توصيل من 4 تجار: {{$per_order_price + ($per_product_price * 4)}} ل.س
             </div>
         </div>
     </div>
@@ -35,7 +35,7 @@
                     <input type="text" name="per_order_price" value="{{$per_order_price}}"/>
                 </div>
                 <div class="input">
-                    <label for="per_product_price">رسم التوصيل على كل منتج</label>
+                    <label for="per_product_price">رسم التوصيل على كل تاجر</label>
                     <input type="text" name="per_product_price" value="{{$per_product_price}}"/>
                 </div>
                 <button type="submit" class="button-primary mt-4 sn-heading-3">حفظ</button>
